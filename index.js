5fb88302821fc1a04fcde651d01f0ed094257668
Milestone AI <47553718+swavafup@users.noreply.github.com>
2023-08-29 10:02:19 +0400

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const textGeneration = async (prompt) => {
 
     try {
         const response = await openai.createCompletion({
-            model: 'text-davinci-003',
+            model: 'gpt-4',
             prompt: `Human: ${prompt}\nAI: `,
             temperature: 0.5,
             max_tokens: 100,
